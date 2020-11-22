09b84d475536eb81055345fc9dacd86aeb9ef21c
Youngkuk Sohn <youngkuk.sohn@gmail.com>
2020-11-22 11:49:30 +0900

    Spring + Mybatis + H2DBMS

--- src/main/java/com/quick/spring/project/repository/MybatisMemberRepository.java
+++ src/main/java/com/quick/spring/project/repository/MybatisMemberRepository.java
@@ -1,32 +1,30 @@
 package com.quick.spring.project.repository;
 
 import com.quick.spring.project.domain.Member;
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.EntityManager;
 import java.util.List;
-/*
+
 @Repository
-public class JpaMemberRepository implements MemberRepository {
-    EntityManager em;
-
+public class MybatisMemberRepository implements MemberRepository {
+    
+    SqlSession sqlSession;
     @Autowired
-    public JpaMemberRepository(EntityManager em) {
-        this.em = em;
+    public MybatisMemberRepository(SqlSession sqlSession) {
+        this.sqlSession = sqlSession;
     }
-
+    
     @Transactional
     @Override
     public long insert(Member member) {
-        em.persist(member);
-        return 0;
+        return sqlSession.insert("Member.save", member);
     }
 
     @Override
     public List<Member> findAll() {
-        return em.createQuery("SELECT m FROM Member m", Member.class).getResultList();
+        return sqlSession.selectList("Member.findAll");
     }
 }
-*/
